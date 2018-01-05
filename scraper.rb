18b0f5f53063362da1a017b6260f1b168d96ee09
Matias Garcia Isaia <mgarcia@manas.tech>
2018-01-05 18:14:09 -0300

    Try to test

--- scraper.rb
+++ scraper.rb
@@ -1,33 +1,25 @@
-#!/usr/bin/env ruby
+# This is a template for a Ruby scraper on morph.io (https://morph.io)
+# including some code snippets below that you should find helpful
+
 require 'scraperwiki'
 require 'mechanize'
-# require 'pry-byebug'
-require 'json'
 
 agent = Mechanize.new
 
-agent.request_headers = { 'accept' => 'application/vnd.bountysource+json; version=2'}
-
-# Certs downloaded from https://curl.haxx.se/ca/cacert.pem
-# as published in https://curl.haxx.se/docs/caextract.html
-
-# Update CA certs
-agent.agent.http.ca_file = File.expand_path 'cacert.pem'
-
 # Read in a page
-page = agent.get("https://api.bountysource.com/teams/crystal-lang")
+page = agent.get("http://foo.com")
+#
+# # Find somehing on the page using css selectors
+# p page.at('div.content')
+#
+# # Write out to the sqlite database using scraperwiki library
+ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
+#
+# # An arbitrary query against the database
+# ScraperWiki.select("* from data where 'name'='peter'")
 
-# Find somehing on the page using css selectors
-data = JSON.parse(page.body)
-
-puts JSON.pretty_generate(data)
-
-# Write out to the sqlite database using scraperwiki library
-ScraperWiki.save_sqlite(["timestamp"], {
-  "timestamp" => Time.now.strftime('%Y%m%d_%H%M%S'),
-  "activity_total" => data['activity_total'],
-  "support_level_sum" => data['support_level_sum'],
-  "support_level_count" => data['support_level_count'],
-  "monthly_contributions_sum" => data['monthly_contributions_sum'],
-  "monthly_contributions_count" => data['monthly_contributions_count'],
-})
+# You don't have to do things with the Mechanize or ScraperWiki libraries.
+# You can use whatever gems you want: https://morph.io/documentation/ruby
+# All that matters is that your final data is written to an SQLite database
+# called "data.sqlite" in the current working directory which has at least a table
+# called "data".
